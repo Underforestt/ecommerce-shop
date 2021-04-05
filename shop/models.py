30edd63618e40dc0454a5ad3d42bc11ffbbc95e0
Yaroslav Pidlisny <slavles04@gmail.com>
2021-04-05 15:15:08 +0300

    Product model functionality added

--- shop/models.py
+++ shop/models.py
@@ -6,3 +6,20 @@ from django.db import models
 class Category(models.Model):
 
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=15, null=True)
+    image = models.ImageField(upload_to='product_images/')
+
+    def display_price(self):
+        return str(self.price) + '$'
+
+    def __str__(self):
+        return self.name
+
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
+from .models import Category, Product
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects
+    products = Product.objects
+    return render(request, 'home.html', {'categories': categories, 'products': products})
 
 
 def product(request):
-    return render(request, 'product.html')
+    categories = Category.objects
+    return render(request, 'product.html', {'categories': categories})
